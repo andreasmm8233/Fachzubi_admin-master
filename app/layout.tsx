e7077b64bb6e56b8eb2f18cdb1e13d389b3b0043
Aman Tomar <tomararpit9@gmail.com>
2026-06-03 18:39:46 +0530

    feat: initialize root layout with dynamic title and Redux provider setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // RootLayout.tsx
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import ThemeCustomization from "@/themes";
@@ -12,10 +12,22 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [title, setTitle] = useState("fachzubi");
+
+  useEffect(() => {
+    const hideLogoDomain = process.env.NEXT_PUBLIC_HIDE_LOGO_DOMAIN;
+    if (typeof window !== "undefined" && hideLogoDomain) {
+      if (window.location.hostname === hideLogoDomain) {
+        const domainName = hideLogoDomain.split(".")[0];
+        setTitle(domainName || "stellenapp");
+      }
+    }
+  }, []);
+
   return (
     <html lang="en">
       <head>
-        <title>fachzubi</title>
+        <title>{title}</title>
       </head>
       <Provider store={store}>
         <ThemeCustomization>
